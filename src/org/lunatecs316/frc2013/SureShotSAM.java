4c2dde778e93cfe4c8a33ff2aa7a4a39e9eb8367
DomenicP <domenicpaul@me.com>
2013-05-08 12:08:13 -0400

    Add Shooter subsystem

--- src/org/lunatecs316/frc2013/SureShotSAM.java
+++ src/org/lunatecs316/frc2013/SureShotSAM.java
@@ -9,23 +9,14 @@ package org.lunatecs316.frc2013;
 
 import org.lunatecs316.frc2013.subsystems.*;
 
-import org.lunatecs316.frc2013.lib.LuNaDrive;
-import org.lunatecs316.frc2013.lib.Potentiometer;
-import org.lunatecs316.frc2013.lib.Tachometer;
 import org.lunatecs316.frc2013.lib.Util;
 
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.Preferences;
-import edu.wpi.first.wpilibj.Relay;
-import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.Victor;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -40,31 +31,12 @@ public class SureShotSAM extends IterativeRobot {
     private Joystick driverController = new Joystick(RobotMap.DRIVER_JOYSTICK);
     private Joystick operatorJoystick = new Joystick(RobotMap.OPERATOR_JOYSTICK);
     
-    // Shooter
-    private Victor shooterMotor = new Victor(RobotMap.SHOOTER_MOTOR);
-    private Tachometer shooterSpeedTach = new Tachometer(RobotMap.SHOOTER_SPEED_TACH);
-    private PIDController shooterSpeedController = new PIDController(-0.005, 0.000, 0.0,
-            shooterSpeedTach, shooterMotor);
-    private Jaguar shooterAngleMotor = new Jaguar(RobotMap.SHOOTER_ANGLE_MOTOR);
-    private Potentiometer shooterAnglePot = new Potentiometer(RobotMap.SHOOTER_ANGLE_POT);
-    private PIDController shooterAngleController = new PIDController(22.5, 0.0, 0.0,
-            shooterAnglePot, shooterAngleMotor);
-    private Solenoid shooterSolenoid = new Solenoid(RobotMap.SHOOTER_SOLENOID);
-    
-    // Climbing
-    private Solenoid climbingSolenoid = new Solenoid(RobotMap.CLIMBING_SOLENOID);
-    
     // Compressor
     private Compressor compressor = new Compressor(RobotMap.COMPRESSOR_PRESSURE_SWITCH,
             RobotMap.COMPRESSOR_RELAY);
     
     // Robot preferences
     private Preferences preferences = Preferences.getInstance();
-    
-    // Variables
-    private static final double SHOOTER_TOP_POSITION = 3.41;
-    private static final double SHOOTER_MID_POSITION = SHOOTER_TOP_POSITION - 0.1;
-    private static final double SHOOTER_LOAD_POSITION = 2.0;
     
     private int autoMode = 1;
     private int autoStep = 1;
@@ -75,28 +47,14 @@ public class SureShotSAM extends IterativeRobot {
      * used for any initialization code.
      */
     public void robotInit() {
+        // Initialize subsystems
         Drivetrain.init();
         Pickup.init();
         Shooter.init();
         Climber.init();
         
-        // Configure sensors
-        
-        
-        shooterSpeedTach.start();
-        
         // Start the compressor
         compressor.start();
-        
-        // Setup LiveWindow
-        
-        LiveWindow.addActuator("Shooter", "Motor", shooterMotor);
-        LiveWindow.addActuator("Shooter", "SpeedController", shooterSpeedController);
-        LiveWindow.addActuator("Shooter", "AngleMotor", shooterAngleMotor);
-        LiveWindow.addActuator("Shooter", "AngleController", shooterAngleController);
-        LiveWindow.addActuator("Shooter", "Solenoid", shooterSolenoid);
-        LiveWindow.addSensor("Shooter", "AnglePot", shooterAnglePot);
-        LiveWindow.addSensor("Shooter", "SpeedTach", shooterSpeedTach);
       
         System.out.println("robotInit() Done!");
     }
@@ -130,23 +88,8 @@ public class SureShotSAM extends IterativeRobot {
         switch(autoStep) {
             case 1:
                 // Start the shooter motor
-                shooterSpeedController.setSetpoint(4500);
-                shooterSpeedController.enable();
-                
-                if (shooterSpeedController.getError() < 300) {
-                    autoStartTime = System.currentTimeMillis();
-                    autoStep++;
-                }
-                break;
             case 2:
                 // Fire the first shot
-                shooterSolenoid.set(true);
-                
-                if (System.currentTimeMillis() - autoStartTime >= 300) {
-                    autoStartTime = System.currentTimeMillis();
-                    autoStep++;
-                }
-                break;
             default:
                 break;
         }
@@ -191,32 +134,24 @@ public class SureShotSAM extends IterativeRobot {
         
         // Angle control
         if (operatorJoystick.getRawButton(11)) {
-            shooterAngleController.setSetpoint(SHOOTER_TOP_POSITION);
-            shooterAngleController.enable();
+            Shooter.moveToPosition(Shooter.Position.Top);
         } else if (operatorJoystick.getRawButton(10)) {
-            shooterAngleController.setSetpoint(SHOOTER_MID_POSITION);
-            shooterAngleController.enable();
+            Shooter.moveToPosition(Shooter.Position.Mid);
         } else if (operatorJoystick.getRawButton(8)) {
-            shooterAngleController.setSetpoint(SHOOTER_LOAD_POSITION);
-            shooterAngleController.enable();
+            Shooter.moveToPosition(Shooter.Position.Load);
         } else {
-            shooterAngleController.disable();
-            shooterAngleMotor.set(Util.deadband(operatorJoystick.getY(), 0.2));
+            Shooter.move(Util.deadband(operatorJoystick.getY(), 0.2));
         }
         
         // Motor control
         if (operatorJoystick.getRawButton(2)) {
-            shooterSpeedController.setSetpoint(3750);
-            shooterSpeedController.enable();
-        } else if (operatorJoystick.getRawButton(5)) {
-            shooterSpeedController.disable();
-            shooterMotor.set(0.5);
+            Shooter.enable();
         } else {
-            shooterSpeedController.disable();
+            Shooter.disable();
         }
         
         // Firing control
-        shooterSolenoid.set(operatorJoystick.getRawButton(1));
+        Shooter.fire(operatorJoystick.getRawButton(1));
         
         /**********************************************************************
          * Climbing
--- src/org/lunatecs316/frc2013/subsystems/Shooter.java
+++ src/org/lunatecs316/frc2013/subsystems/Shooter.java
@@ -4,10 +4,113 @@
  */
 package org.lunatecs316.frc2013.subsystems;
 
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
+import org.lunatecs316.frc2013.RobotMap;
+import org.lunatecs316.frc2013.lib.Potentiometer;
+import org.lunatecs316.frc2013.lib.Tachometer;
+
 /**
  *
  * @author domenicpaul
  */
 public class Shooter {
+    /**
+     * Represents different positions the shooter can move to
+     */
+    public static class Position {
+        private double val;
+        
+        public static final Position Top = new Position(3.41);
+        public static final Position Mid = new Position(3.31);
+        public static final Position Load = new Position(2.0);
+        
+        // Private to prevent creation of other positions
+        private Position(double val) {
+            this.val = val;
+        }
+        
+        /**
+         * Get the numeric value for the PIDController
+         * @return - the double value of the setpoint
+         */
+        public double getVal() {
+            return val;
+        }
+    }
     
+    private static final Victor motor = new Victor(RobotMap.SHOOTER_MOTOR);
+    private static final Tachometer speedTach = new Tachometer(RobotMap.SHOOTER_SPEED_TACH);
+    private static final PIDController speedController = new PIDController(-0.005,
+            0.000, 0.0, speedTach, motor);
+    
+    private static final Jaguar angleMotor = new Jaguar(RobotMap.SHOOTER_ANGLE_MOTOR);
+    private static final Potentiometer anglePot = new Potentiometer(RobotMap.SHOOTER_ANGLE_POT);
+    private static final PIDController angleController = new PIDController(22.5,
+            0.0, 0.0, anglePot, angleMotor);
+    
+    private static final Solenoid solenoid = new Solenoid(RobotMap.SHOOTER_SOLENOID);
+    
+    // Private to prevent creation of an instance
+    private Shooter() {}
+    
+    /**
+     * Setup the subsystem
+     */
+    public static void init() {
+        speedController.setSetpoint(3975);
+        speedTach.start();
+        
+        LiveWindow.addActuator("Shooter", "Motor", motor);
+        LiveWindow.addActuator("Shooter", "SpeedController", speedController);
+        LiveWindow.addActuator("Shooter", "AngleMotor", angleMotor);
+        LiveWindow.addActuator("Shooter", "AngleController", angleController);
+        LiveWindow.addActuator("Shooter", "Solenoid", solenoid);
+        LiveWindow.addSensor("Shooter", "AnglePot", anglePot);
+        LiveWindow.addSensor("Shooter", "SpeedTach", speedTach);
+    }
+    
+    /**
+     * Move the shooter to the specified position
+     * @param pos 
+     */
+    public static void moveToPosition(Position pos) {
+        angleController.setSetpoint(pos.getVal());
+        angleController.enable();
+    }
+    
+    /**
+     * Manually control the angle
+     * @param val - the movement speed value
+     */
+    public static void move(double val) {
+        angleController.disable();
+        angleMotor.set(val);
+    }
+    
+    /**
+     * Enable the shooter wheel
+     */
+    public static void enable() {
+        speedController.enable();
+    }
+    
+    /**
+     * Disable the shooter wheel
+     */
+    public static void disable() {
+        speedController.disable();
+    }
+    
+    /**
+     * Fire the frisbee
+     * @param value - whether to shoot or not
+     */
+    public static void fire(boolean value) {
+        solenoid.set(value);
+    }
 }
